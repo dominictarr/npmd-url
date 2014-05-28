229b5c7ec87a350ec68c99dadd37d981ad02f816
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-28 03:06:20 +0200

    test coverage for tarballUrl property

--- test/index.js
+++ test/index.js
@@ -14,6 +14,8 @@ t('curry@1.2.0', 'https://registry.npmjs.org/curry/-/curry-1.2.0.tgz')
 t('highlight.js@8.0.0', 'https://registry.npmjs.org/highlight.js/-/highlight.js-8.0.0.tgz')
 t('JSONStream@6.2.0', 'https://registry.npmjs.org/JSONStream/-/JSONStream-6.2.0.tgz')
 
+t({tarballUrl: 'https://registry.npmjs.org/JSONStream/-/JSONStream-6.2.0.tgz'},
+  'https://registry.npmjs.org/JSONStream/-/JSONStream-6.2.0.tgz')
 //git urls are already tested in npm-github-url-resolve
 
 tape('custom registry', function(t){
